030d629504483a09e0f9609ab04defdedbbdb22a
rishabnayak <rishab@bu.edu>
2020-03-07 17:51:26 -0500

    save?

--- src/components/ApplicationUI.vue
+++ src/components/ApplicationUI.vue
@@ -10,15 +10,17 @@
         label="Name"
         placeholder="John Doe"
       ></v-text-field>
-      <label>Place of Incarceration</label>
+      <v-file-input
+        chips
+        multiple
+        label="Resume Upload"
+        v-model="uploadedResume"
+      ></v-file-input>
       <v-select
-        :items="prisons"
-        v-model="location"
-        label="Select the prison (required)"
+        :items="educationLevels"
+        v-model="educationLevel"
+        label="Select your Level of Education (required)"
       ></v-select>
-      <v-btn type="submit" color="primary" class="mr-4" @click="saveApplication"
-        >Save</v-btn
-      >
       <v-btn
         type="submit"
         color="primary"
@@ -36,6 +38,9 @@
         >Submit</v-btn
       >
     </v-form>
+    <v-btn color="primary" class="mr-4" @click.stop="saveApplication"
+      >Save</v-btn
+    >
   </v-container>
 </template>
 <script>
@@ -49,10 +54,20 @@ export default {
       age: null,
       gender: null,
       prounouns: null,
+      educationLevels: [
+        "High School",
+        "College Freshman",
+        "College Sophomore",
+        "College Junior",
+        "College Senior",
+        "Masters",
+        "PhD"
+      ],
       educationLevel: null,
       university: null,
       major: null,
       minor: null,
+      uploadedResume: null,
       resume: null,
       githubURL: null,
       linkedinURL: null,
@@ -69,7 +84,35 @@ export default {
       return this.$store.state.user;
     }
   },
-  methods: {},
+  methods: {
+    async saveApplication() {
+      await functions.httpsCallable("uploadResume")({
+        file: this.uploadedResume[0]
+      });
+      console.log(this.uploadedResume[0]);
+      //   await functions.httpsCallable("saveApplication")({
+      //     uid: this.user.uid,
+      //     name: this.name,
+      //     phone: this.phone,
+      //     age: this.age,
+      //     gender: this.gender,
+      //     prounouns: this.pronouns,
+      //     educationLevel: this.educationLevel,
+      //     university: this.university,
+      //     major: this.major,
+      //     minor: this.minor,
+      //     resume: this.resume,
+      //     githubURL: this.githubURL,
+      //     linkedinURL: this.linkedinURL,
+      //     otherURL: this.otherURL,
+      //     beenToHackathon: this.beenToHackathon,
+      //     attendedBHacks: this.attendedBHacks,
+      //     marketingData: this.marketingData,
+      //     tAndC1: this.tAndC1,
+      //     tAndC2: this.tAndC2
+      //   });
+    }
+  },
   async mounted() {
     if (!this.user.applicationStatus) {
       await functions.httpsCallable("createApplication")({
