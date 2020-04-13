9646b9672fa09da769dee3d7ff2c1dff94f2fd21
Angela Tran <angela.tran1220@gmail.com>
2020-04-13 14:32:54 -0400

    Beginning text

--- src/components/LandingPage.vue
+++ src/components/LandingPage.vue
@@ -11,12 +11,25 @@
           ></v-img>
         </v-flex>
         <v-flex mb-4>
-          <h1 class="display-2 font-weight-bold mb-3">Welcome to Cacioepepe</h1>
+          <h2
+            class="display-1 indigo--text text--darken-4 font-weight-bold mb-3"
+          >
+            We're turning 5.
+          </h2>
+        </v-flex>
+        <v-flex mb-4>
+          <h3
+            class="display-1 red--text text--accent-2 font-weight-medium mb-3"
+          >
+            And you're invited!
+          </h3>
         </v-flex>
       </v-layout>
     </v-container>
   </v-layout>
 </template>
+
+"display-2 font-weight-bold mb-3"
 
 <script>
 export default {
