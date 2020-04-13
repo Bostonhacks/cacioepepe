86aa5e6cb2b76777284d8fb5932ab2268d39c0f8
Angela Tran <angela.tran1220@gmail.com>
2020-04-13 17:18:21 -0400

    Top navigation bar and button

--- src/components/LandingPage.vue
+++ src/components/LandingPage.vue
@@ -1,15 +1,15 @@
 <template>
   <v-layout align-center justify-center>
+    <v-app-bar absolute color="indigo darken-4" dark>
+      <v-tabs right>
+        <v-tab>Schedule</v-tab>
+        <v-tab>Tracks and Workshops</v-tab>
+        <v-tab>FAQ</v-tab>
+      </v-tabs>
+    </v-app-bar>
+
     <v-container>
       <v-layout text-center wrap>
-        <v-flex xs12>
-          <v-img
-            :src="require('../assets/logo.svg')"
-            class="my-3"
-            contain
-            height="200"
-          ></v-img>
-        </v-flex>
         <v-flex mb-4>
           <h2
             class="display-1 indigo--text text--darken-4 font-weight-bold mb-3"
@@ -17,6 +17,9 @@
             We're turning 5.
           </h2>
         </v-flex>
+      </v-layout>
+
+      <v-layout text-center wrap>
         <v-flex mb-4>
           <h3
             class="display-1 red--text text--accent-2 font-weight-medium mb-3"
@@ -25,11 +28,15 @@
           </h3>
         </v-flex>
       </v-layout>
+
+      <v-layout text-center wrap>
+        <v-flex mb-4>
+          <v-btn rounded color="deep-orange lighten-3" dark>Login</v-btn>
+        </v-flex>
+      </v-layout>
     </v-container>
   </v-layout>
 </template>
-
-"display-2 font-weight-bold mb-3"
 
 <script>
 export default {
