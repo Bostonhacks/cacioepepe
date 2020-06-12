659c9031d1150c1916326f850d9f0aba72eb0eb2
asifrahman479 <rasif@bu.edu>
2020-06-11 20:36:07 -0400

    fix updateWifi

--- functions/adminFunctions/updateWifiDetails.function.js
+++ functions/adminFunctions/updateWifiDetails.function.js
@@ -2,7 +2,7 @@ const admin = require("firebase-admin");
 const functions = require("firebase-functions");
 
 const db = admin.firestore();
-
+// updateWifiDetails({name: "A", password: "12"})
 module.exports.updateWifiDetails = functions.https.onCall(
   async (data, context) => {
     if (!context.auth) {
