359cf46a2c6b1d7c7910b95ca51e1e2e70619ad3
officialjasonkim <jasonk@bu.edu>
2020-07-12 17:39:39 -0400

    landing updates

--- src/components/common/MobileBar.vue
+++ src/components/common/MobileBar.vue
@@ -1,6 +1,6 @@
 <template>
   <nav>
-    <v-app-bar>
+    <v-app-bar style="background: #80d2ff !important" elevation="0">
       <v-app-bar-nav-icon @click="drawer = !drawer"></v-app-bar-nav-icon>
       <img
         class="mr-3"
