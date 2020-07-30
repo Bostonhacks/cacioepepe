e8a52c6e390881dbdf96d16c5a0f12bd2cafe500
Yuanming Chai <yaunmingchai@gmail.com>
2020-07-29 21:40:54 -0400

    functions

--- functions/adminFunctions/adminApplicationFunctions/readDeadline.function.js
+++ functions/adminFunctions/adminApplicationFunctions/readDeadline.function.js
@@ -7,12 +7,14 @@ module.exports.readDeadline = functions.https.onCall(async (_, context) => {
   if (!context.auth) {
     return { message: "Authentication Required!", code: 401 };
   }
-
-  const deadlineDb = db.collection("admin").doc("regDeadline");
-  var deadline = await deadlineDb.get();
   var res = [];
-  deadline.forEach(element => {
-    res.push(element.data());
+  const deadlineDb = db.collection("admin").doc("regDeadline");
+  deadlineDb.get().then(function(doc) {
+    if (doc.exists) {
+      res.push(doc.data());
+    } else {
+      console.log("No such document!");
+    }
   });
   return res;
 });
