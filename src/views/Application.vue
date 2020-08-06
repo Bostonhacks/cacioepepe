b57dc8f209129bf23569f3b8d0bc817c6d930cb8
Yuanming Chai <yaunmingchai@gmail.com>
2020-08-06 16:33:57 -0400

    minor issue fix

--- src/views/Application.vue
+++ src/views/Application.vue
@@ -1,5 +1,5 @@
 <template>
-  <div v-if="'date' < 'deadline'">
+  <div v-if="'parseDate(date)' > 'parseDate(dealine)'">
     <RefuseUI />
   </div>
   <div v-else>
@@ -10,12 +10,31 @@
 <script>
 import ApplicationUI from "@/components/hacker/ApplicationUI";
 import RefuseUI from "@/components/reject/RefuseUI";
+import { functions } from "@/firebase/init";
 
 export default {
   data() {
     return {
-      date: new Date()
+      date: new Date().toISOString().substr(0, 10),
+      deadline: ""
     };
+  },
+  async mounted() {
+    this.getDate();
+  },
+  methods: {
+    parseDate(date) {
+      if (!date) return null;
+
+      const [month, day, year] = date.split("/");
+      return `${year}-${month.padStart(2, "0")}-${day.padStart(2, "0")}`;
+    },
+    async getDate() {
+      var out = await functions.httpsCallable("readDeadline")({});
+      this.deadline = out.data["finishTime"];
+      console.log(this.date);
+      console.log(this.deadline);
+    }
   },
   components: {
     ApplicationUI,
