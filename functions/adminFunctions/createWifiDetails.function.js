8916fb3180d2dbc62d4110e217d8bf640c10757f
Asif Rahman <asifrahman479@gmail.com>
2020-08-11 21:54:38 -0400

    Asif (#88)
    * calendar scheduling and event scheduling in process
    
    * redundant code removed
    
    * event functions
    
    * added authentication
    
    * update wifi details fnctions created
    
    * fix updateWifi
    
    * added CRUD for FAQs
    
    * added CRUD for prizes
    
    * updated CRUD for FAQs
    
    * added CRUS for WifiDetails
    
    * added functions for wifi and devposts
    
    Co-authored-by: Jin Young Bang <jybang@bu.edu>
    Co-authored-by: nafisrahman <nafisrahman797@gmail.com>

--- functions/adminFunctions/createWifiDetails.function.js
+++ functions/adminFunctions/createWifiDetails.function.js
@@ -1,8 +1,9 @@
 const admin = require("firebase-admin");
 const functions = require("firebase-functions");
-const { v4: uuidv4 } = require("uuid");
 
 const db = admin.firestore();
+
+const arrayUnion = admin.firestore.FieldValue.arrayUnion;
 
 module.exports.createWifiDetails = functions.https.onCall(
   async (data, context) => {
@@ -10,15 +11,20 @@ module.exports.createWifiDetails = functions.https.onCall(
       return { message: "Authentication Required!", code: 401 };
     }
 
-    var uid = uuidv4();
-    const file = db.collection("wifiDetails").doc(uid);
-
-    await file.set({
-      uid: uid,
+    var newWifi = {
       name: data.name,
       password: data.password
-    });
+    };
+    const wifiDb = db.collection("admin").doc("wifiDetails");
 
-    return "Completed!";
+    await wifiDb
+      .update({
+        wifis: arrayUnion(newWifi)
+      })
+      .catch(function(error) {
+        console.error("Error: ", error);
+      });
+
+    return { message: "New wifi created", code: 201 };
   }
 );
