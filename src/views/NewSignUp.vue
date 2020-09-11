100f597ee9d35a56d8842f277feac3c63fd9861c
Yuanming Chai <yaunmingchai@gmail.com>
2020-09-10 22:59:52 -0400

    new signup page

---
+++ src/views/NewSignUp.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <SignUp class="google-signup" />
+  </div>
+</template>
+
+<style scoped>
+.google-signup {
+  margin-top: 200px;
+}
+</style>
+
+<script>
+import SignUp from "@/components/login/SignUp";
+
+export default {
+  components: {
+    SignUp
+  }
+};
+</script>
