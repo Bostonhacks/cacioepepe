3f3a7eb7ed021f00a1b71ff9a4c05a3576fce9d4
Xavier Ruiz <xruiz@Xaviers-MacBook-Pro.local>
2020-09-28 07:03:39 -0400

    got a basic agenda and logos yeeeeet

--- src/components/common/NavigationBar.vue
+++ src/components/common/NavigationBar.vue
@@ -9,6 +9,16 @@
           <Banner :bannerLink="bannerLink" />
         </button>
       </li>
+      <li>
+        <a
+          href="https://mlh.io/seasons/2021/events?utm_source=na-hackathon&utm_medium=TrustBadge&utm_campaign=2021-season&utm_content=white"
+          target="_blank"
+          ><img
+            src="https://s3.amazonaws.com/logged-assets/trust-badge/2021/mlh-trust-badge-2021-white.svg"
+            alt="Major League Hacking 2021 Hackathon Season"
+            style="width:100%"
+        /></a>
+      </li>
     </ul>
   </nav>
 </template>
