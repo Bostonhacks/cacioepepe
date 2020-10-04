1f382b19a10bcb8c46ab8f3932f04aa0d211307f
Jin Young Bang <jybang@bu.edu>
2020-10-04 17:58:52 -0400

    Created separate delete resume functions

---
+++ functions/sponsorFunctions/deleteSelectedResumes.function.js
@@ -0,0 +1,20 @@
+const functions = require("firebase-functions");
+const { Storage } = require("@google-cloud/storage");
+const storage = new Storage();
+
+module.exports.deleteSelectedResumes = functions
+  .runWith({ timeoutSeconds: 540, memory: "2GB" })
+  .https.onCall(async (data, context) => {
+    if (!context.auth) {
+      return { message: "Authentication Required!", code: 401 };
+    }
+
+    const bucket = storage.bucket("bostonhacks-cacioepepe.appspot.com");
+    var path = "selectedResumes/" + context.auth.uid + "/" + "Resume.zip";
+    await bucket.file(path).delete();
+
+    return {
+      message: "Selected resumes deleted.",
+      code: 200
+    };
+  });
