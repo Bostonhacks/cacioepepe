fe465366d93c300a3ddf79f3b856390610b35017
Vitor Vicente <vitor@bu.edu>
2020-10-23 10:18:33 -0400

    Volunteer Dashboard

---
+++ src/components/volunteer/SlackChannels.vue
@@ -0,0 +1,35 @@
+<template>
+  <v-container class="justify-center">
+    <v-card max-width="450" class="mx-auto">
+      <v-toolbar color="orange" dark>
+        <v-app-bar-nav-icon></v-app-bar-nav-icon>
+
+        <v-toolbar-title>Slack Channels</v-toolbar-title>
+
+        <v-spacer></v-spacer>
+      </v-toolbar>
+      <v-list-item three-line>
+        <v-list-item-content>
+          <v-list-item-subtitle class="pd-10"
+            >Communicate with hundreds of hackers! Reach out to learn about our
+            workshops, update with announcements!</v-list-item-subtitle
+          >
+          <v-card-actions>
+            <v-list>
+              <v-list-item>
+                <v-btn block color="secondary" dark
+                  >Open #general Channel</v-btn
+                >
+              </v-list-item>
+              <v-list-item>
+                <v-btn block color="secondary" dark
+                  >Open #announcements Channel</v-btn
+                >
+              </v-list-item>
+            </v-list>
+          </v-card-actions>
+        </v-list-item-content>
+      </v-list-item>
+    </v-card>
+  </v-container>
+</template>
