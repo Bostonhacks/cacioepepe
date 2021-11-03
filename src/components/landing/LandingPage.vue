6f98465a93f4cc9fc9a60dba851edb36634b807c
Jordan Nichols <jordan.jbns@gmail.com>
2021-11-03 11:10:13 -0400

    Sponsorlogos (#217)
    * sketch and repit need to be fixed
    
    * fixed replit,sketch,taskade logos
    
    * Added logos to landing page
    
    Co-authored-by: Yuanming Chai <yaunmingchai@gmail.com>
    Co-authored-by: Pan-Tom <panenko.tom@gmail.com>

--- src/components/landing/LandingPage.vue
+++ src/components/landing/LandingPage.vue
@@ -1,49 +1,79 @@
 <template>
   <div class="overflow-wrapper">
-    <v-container>
-      <v-row justify="center" class="my-16">
-        <v-col cols="12" lg="4"><Logo /></v-col>
-        <v-col cols="0" lg="1"></v-col>
-        <v-col cols="12" lg="4"> <Section1 /></v-col>
-      </v-row>
-      <v-row v-if="!user" justify="center" class="my-16">
-        <v-col cols="5" lg="2" class="d-flex justify-center"
-          ><LoginButton />
-        </v-col>
-        <v-col cols="5" lg="2" class="d-flex justify-center"
-          ><SignupButton />
-        </v-col>
-      </v-row>
-      <v-row v-else justify="center" class="my-16">
-        <v-col cols="5" lg="2" class="d-flex justify-center"
-          ><LogoutButton />
-        </v-col>
-        <v-col cols="5" lg="2" class="d-flex justify-center"
-          ><ApplyButton
-            v-if="user.applicationStatus == 0 || !user.applicationStatus"
-          />
-          <DashboardButton v-else />
-        </v-col>
-      </v-row>
+    <v-row justify="center" class="my-16">
+      <v-col cols="12" lg="4"><Logo /></v-col>
+      <v-col cols="0" lg="1"></v-col>
+      <v-col cols="12" lg="4"> <Section1 /></v-col>
+    </v-row>
+    <v-row v-if="!user" justify="center" class="my-16">
+      <v-col cols="5" lg="2" class="d-flex justify-center"
+        ><LoginButton />
+      </v-col>
+      <v-col cols="5" lg="2" class="d-flex justify-center"
+        ><SignupButton />
+      </v-col>
+    </v-row>
+    <v-row v-else justify="center" class="my-16">
+      <v-col cols="5" lg="2" class="d-flex justify-center"
+        ><LogoutButton />
+      </v-col>
+      <v-col cols="5" lg="2" class="d-flex justify-center"
+        ><ApplyButton
+          v-if="user.applicationStatus == 0 || !user.applicationStatus"
+        />
+        <DashboardButton v-else />
+      </v-col>
+    </v-row>
 
-      <v-row class="my-16">
-        <v-col cols="0" lg="1"></v-col>
-        <v-col cols="12" lg="6"><Schedule /></v-col>
-      </v-row>
-      <v-row justify="center" class="my-16">
-        <v-col cols="10" lg="3"><Track1 /></v-col>
-        <v-col cols="10" lg="3"><Track2 /></v-col>
-        <v-col cols="10" lg="3"><Track3 /></v-col>
-      </v-row>
-      <v-row class="flex-row-reverse my-16">
-        <v-col cols="0" lg="1"></v-col>
-        <v-col cols="12" lg="6"><FAQ /></v-col>
-      </v-row>
-      <!-- Wait for Shimli to get logos -->
-      <!-- <v-row justify="center" class="my-16">
-        <v-col cols="12" lg="10"><Sponsors /></v-col>
-      </v-row> -->
-    </v-container>
+    <v-row class="my-16">
+      <v-col cols="0" lg="1"></v-col>
+      <v-col cols="12" lg="6"><Schedule /></v-col>
+    </v-row>
+    <v-row justify="center" class="my-16">
+      <v-col cols="10" lg="3"><Track1 /></v-col>
+      <v-col cols="10" lg="3"><Track2 /></v-col>
+      <v-col cols="10" lg="3"><Track3 /></v-col>
+    </v-row>
+    <v-row class="flex-row-reverse my-16">
+      <v-col cols="0" lg="1"></v-col>
+      <v-col cols="12" lg="6"><FAQ /></v-col>
+    </v-row>
+    <v-row class="mx-12 justify-center justify-lg-space-between">
+      <v-col cols="12" md="6" lg="3" class="my-12">
+        <v-img src="@/components/sponsor/logos/balsamiq.png" />
+      </v-col>
+      <v-col cols="12" md="6" lg="3" class="my-12">
+        <v-img src="@/components/sponsor/logos/SketchLogo.png" />
+      </v-col>
+      <v-col cols="12" md="6" lg="3" class="my-12">
+        <v-img src="@/components/sponsor/logos/GoogleCloud.png" />
+      </v-col>
+    </v-row>
+    <v-row class="mx-12 justify-center justify-lg-space-between">
+      <v-col cols="12" md="6" lg="3" class="my-12">
+        <Axure_logo_400 />
+      </v-col>
+      <v-col cols="12" md="6" lg="3" class="my-12">
+        <v-img
+          margin-top="10px"
+          src="@/components/sponsor/logos/thunkableLogo.png"
+        />
+      </v-col>
+      <v-col cols="12" md="6" lg="3" class="my-12">
+        <v-img src="@/components/sponsor/logos/ReplitLogo.png" />
+      </v-col>
+    </v-row>
+    <v-row class="mx-12 justify-center justify-lg-space-between">
+      <v-col cols="12" md="6" lg="3" class="my-12">
+        <v-img src="@/components/sponsor/logos/RStudio-Logo-Gray.png" />
+      </v-col>
+      <v-col cols="12" md="6" lg="3" class="my-12">
+        <v-img src="@/components/sponsor/logos/echoar.png" />
+      </v-col>
+      <v-col cols="12" md="6" lg="3" class="my-12">
+        <taskade_logo />
+      </v-col>
+    </v-row>
   </div>
 </template>
 
@@ -61,8 +91,8 @@ import FAQ from "@/components/landing/FAQ.vue";
 import Track1 from "@/components/landing/Track1.vue";
 import Track2 from "@/components/landing/Track2.vue";
 import Track3 from "@/components/landing/Track3.vue";
-// import Sponsors from "@/components/landing/Sponsors.vue";
-
+import taskade_logo from "@/components/sponsor/logos/Taskade_logo.vue";
+import Axure_logo_400 from "@/components/sponsor/logos/Axure_logo_400.vue";
 export default {
   computed: {
     user() {
@@ -81,8 +111,9 @@ export default {
     Track1,
     Track2,
     Track3,
-    // Sponsors,
-    FAQ
+    FAQ,
+    taskade_logo,
+    Axure_logo_400
   },
   async mounted() {
     this.getEvents();
